9122d67da4937c6146bdb33755068b75f0d3383f
Jared Tobin <jared@jtobin.io>
2026-01-11 10:27:15 +0400

    flake: update

--- flake.nix
+++ flake.nix
@@ -8,6 +8,17 @@
       ref  = "master";
       inputs.ppad-nixpkgs.follows = "ppad-nixpkgs";
     };
+    ppad-hkdf = {
+      # XX temporarily using github mirror
+      type = "github";
+      owner = "ppad-tech";
+      repo = "hkdf";
+      # type = "git";
+      # url  = "git://git.ppad.tech/hkdf.git";
+      ref  = "master";
+      inputs.ppad-nixpkgs.follows = "ppad-nixpkgs";
+      inputs.ppad-sha256.follows = "ppad-sha256";
+    };
     ppad-secp256k1 = {
       type = "git";
       url  = "git://git.ppad.tech/secp256k1.git";
@@ -31,7 +42,7 @@
   };
 
   outputs = { self, nixpkgs, flake-utils, ppad-nixpkgs
-            , ppad-aead, ppad-secp256k1, ppad-sha256
+            , ppad-aead, ppad-hkdf, ppad-secp256k1, ppad-sha256
             }:
     flake-utils.lib.eachDefaultSystem (system:
       let
@@ -48,6 +59,12 @@
             (hlib.enableCabalFlag aead "llvm")
             [ llvm clang ];
 
+        hkdf = ppad-hkdf.packages.${system}.default;
+        hkdf-llvm =
+          hlib.addBuildTools
+            (hlib.enableCabalFlag hkdf "llvm")
+            [ llvm clang ];
+
         secp256k1 = ppad-secp256k1.packages.${system}.default;
         secp256k1-llvm =
           hlib.addBuildTools
@@ -62,10 +79,12 @@
 
         hpkgs = pkgs.haskell.packages.ghc910.extend (new: old: {
           ppad-aead = aead-llvm;
+          ppad-hkdf = hkdf-llvm;
           ppad-secp256k1 = secp256k1-llvm;
           ppad-sha256 = sha256-llvm;
           ${lib} = new.callCabal2nix lib ./. {
             ppad-aead = new.ppad-aead;
+            ppad-hkdf = new.ppad-hkdf;
             ppad-secp256k1 = new.ppad-secp256k1;
             ppad-sha256 = new.ppad-sha256;
           };
